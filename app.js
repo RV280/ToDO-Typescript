de4d6c8911bae4f6c0acc0af5d0aaf547663eb44
Rahul Varshney <rahul.varshney@dream11.com>
2022-08-04 18:09:46 +0530

    Initial Modifications

---
+++ app.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("keyup", function(event){
+    if(event.key === "Enter"){
+        addTask();
+    }
+});
+
+document.addEventListener("click", function(event){
+    const element = event.target;
+
+    const elementJob = elemet.attributes
+});
